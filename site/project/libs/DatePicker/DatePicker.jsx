8a46cdc176aba1f3dcb1bdf1fcf20519d799448a
Nikolai Maltsev <nsmalcev@gmail.com>
2022-03-27 01:01:11 +0100

    update datepicker

--- site/project/libs/DatePicker/DatePicker.jsx
+++ site/project/libs/DatePicker/DatePicker.jsx
@@ -1,17 +1,35 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 import s from './DatePicker.module.css';
 
 export default function DatePicker(props) {
-    const [date, setDate] = useState(props.date || new Date());
+    const initDate = props.date || new Date();
+    const [actualDate, setActualDate] = useState(initDate);
+    const [renderedDate, setRenderedDate] = useState(initDate);
     const [isOpened, openCalendar] = useState(false);
+    
     const toggleHandler = useCallback((event) => {
       openCalendar(!isOpened)
     }, [isOpened])
+
+    // To close the calendar after selecting a new date
+    useEffect(() => {
+      console.log('[ACTUALDATE changed] %s/%s', actualDate, renderedDate);
+      if ((actualDate-0) !== (renderedDate-0)) {
+        console.log('[ACTUALDATE changed2]');
+        setRenderedDate(actualDate)
+        openCalendar(false);
+      }
+    }, [setRenderedDate, actualDate, openCalendar])
+
+    // When the calendar closes, the actual date becomes the rendered date
+    useEffect(() => {
+      if (isOpened === true) return;
+      setRenderedDate(actualDate)
+    }, [isOpened, setRenderedDate, actualDate])
+    
     const mouseOutHandler = useCallback((event) => {
-      // console.log('mouseOutHandler %s');
-      // console.dir(event)
-      const 	$target = event.toElement || event.relatedTarget;
+      const $target = event.toElement || event.relatedTarget;
       const conf_target = event.target.closest('.dropdown')
 
       if(!(
@@ -21,33 +39,61 @@ export default function DatePicker(props) {
       }
     }, [isOpened])
 
-    const month = date.getMonth()
-    const year = date.getFullYear()
-    const firstDayNumber = new Date(year, month, 1).getDay();
-    const daysInMonth = Math.floor((new Date(year, month +1, 0) -new Date(year, month, 0)) / (1000*3600*24))
+    const nextClickHandler = useCallback((event) => {
+      setRenderedDate(new Date(renderedDate.getFullYear(), renderedDate.getMonth() + 1))
+    }, [renderedDate])
+    
+    const prevClickHandler = useCallback((event) => {
+      setRenderedDate(new Date(renderedDate.getFullYear(), renderedDate.getMonth() - 1))
+    }, [renderedDate])
 
-    console.log('State');
-    console.dir(date)
+    const onSelectDayHandler = useCallback((event) => {
+      const date = new Date(renderedDate.getFullYear(), renderedDate.getMonth(), event.target.dataset.day-0)
+      setActualDate(date)
+    }, [renderedDate, setActualDate]);
+    
+    const month = renderedDate.getMonth()
+    const year = renderedDate.getFullYear()
+    const firstDayNumber = new Date(year, month, 1).getDay();
+    const daysInMonth = Math.floor((new Date(year, month + 1) - new Date(year, month)) / (1000*3600*24))
+
     return <div className={s.root}>
-      <div onClick={toggleHandler}>{date+''}</div>
+      <div onClick={toggleHandler}>{actualDate+''}</div>
       {isOpened && <div className="dropdown" onMouseOut={mouseOutHandler}>
         <div className={s.dropdown}>
           <div class={s.calendar} style={{'--first-day-number': firstDayNumber}}>
             <div class={s.monthIndicator}>
-              <time datetime="2019-02"> February 2019 </time>
+              <div onClick={prevClickHandler}>Prev</div>
+              <time datetime="2019-02">{year} {month}</time>
+              <div onClick={nextClickHandler}>Next</div>
             </div>
             <div class={s.dayOfWeek}>
-              <div>Su</div>
               <div>Mo</div>
               <div>Tu</div>
               <div>We</div>
               <div>Th</div>
               <div>Fr</div>
               <div>Sa</div>
+              <div>Su</div>
             </div>
-            <div class={s.dateGrid}>
-              {Array(daysInMonth).fill(0).map((_, index) => <button><time datetime="2019-02-01">{index +1}</time></button>)}
-                </div>
+            <div class={s.dateGrid}>{Array(daysInMonth).fill(0)
+              .map((_, index) => {
+                const classList = s.day;
+                if (year === actualDate.getFullYear() 
+                  && month === actualDate.getMonth()
+                  && (index + 1) === actualDate.getDate()  
+                ) {
+                  classList += ' ' + s.actual;
+                }
+
+                return <time 
+                  key={index} 
+                  className={classList} 
+                  onClick={onSelectDayHandler} 
+                  data-day={index + 1} 
+                  datetime="2019-02-01">{index + 1}</time>
+              })
+            }</div>
         </div>
         </div>
       </div>}
