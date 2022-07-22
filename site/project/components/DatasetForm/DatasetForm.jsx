efd5e5cbb8fd9b69dbc66f4843da03a938508a8a
Nikolai Maltsev <nmaltsev@argans.eu>
2022-07-22 10:47:08 +0200

    improuve CSS styles

--- site/project/components/DatasetForm/DatasetForm.jsx
+++ site/project/components/DatasetForm/DatasetForm.jsx
@@ -6,29 +6,30 @@ const RENDER_PPOPERTIES = 'Parameter,Name,type,level,resolution,first obs,last o
 
 function DatasetList(props){
     if (props.datasets.length < 1) return null;
+    // TODO refactor
     const defaultDataset = props.datasets[0];
     
     return (<>
         {RENDER_PPOPERTIES.map((propertyKey) => {
             const value = defaultDataset[propertyKey];
             if (propertyKey === 'link') {
-                return <a href={value} target="_blank">More info</a>
+                return <a href={value} target="_blank" className="with-ellipsis">More info</a>
             }
             else if (propertyKey === 'Parameter') {
                 // TODO render a list of other options for absolute positions
-                return (<div title={value}>{value}</div>)
+                return (<div title={value} className="with-ellipsis">{value}</div>)
             }
             else if (propertyKey === 'Name') {
                 // TODO find icon 
-                return (<div title={value} className="without-ellipsis">
+                return (<div title={value}>
                     <div className="bflex-row">
-                        <span className="flex-size-fill">{value}</span>
+                        <span className="flex-size-fill with-ellipsis">{value}</span>
                         <i className="flex-size-own">[^]</i>
                     </div>
                 </div>)
             }
             
-            return (<div title={value}>{value}</div>)
+            return (<div title={value} className="with-ellipsis">{value}</div>)
         })}
     </>)
 }
@@ -36,17 +37,15 @@ function DatasetList(props){
 export default function DatasetForm(props){
     const [datasets_per_parameter, setDatasetPerParameter] = useState({})
     useEffect(() => {
-        // console.log('[DatasetForm] %s', props.region)
-        // console.dir(datasets_per_region[props.region]);
         setDatasetPerParameter(datasets_per_region[props.region]);
     }, [props.region])
 
     return <fieldset 
-        className="regular-grid with-ellipsis" 
+        className="regular-grid" 
         style={{
-            '--description-grid__columns': headers.map(() => '1fr').join(' ')
+            '--description-grid__columns': '1fr 1fr min-content min-content min-content min-content min-content min-content min-content'
         }}>
-        {headers.map(header => <h4 key={header}>{header}</h4>)}
+        {headers.map(header => <h4 key={header} className="with-ellipsis">{header}</h4>)}
         {datasets_per_parameter && Object
             .values(datasets_per_parameter)
             .map((datasets) => <DatasetList datasets={datasets}/>)}
