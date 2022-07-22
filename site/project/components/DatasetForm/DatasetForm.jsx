b494147798be04b82f01c85606db7a3a85200aa1
Nikolai Maltsev <nmaltsev@argans.eu>
2022-07-22 17:08:40 +0200

    Improve styles of the dataset list component

--- site/project/components/DatasetForm/DatasetForm.jsx
+++ site/project/components/DatasetForm/DatasetForm.jsx
@@ -1,13 +1,14 @@
 import datasets_per_region from 'models/datasets.json';
 import { useCallback, useEffect, useState } from 'react';
+import { classList } from 'utils/strings';
 import S from './DataForm.module.css';
 
 const headers = 'Parameter,Name,Type,Level,resolution,first obs,last obs,Frequency,Link'.split(',');
 const RENDER_PPOPERTIES = 'Parameter,Name,type,level,resolution,first obs,last obs,frequency,link'.split(',');
 
-function Link({value}) {
-    if (!value) return <span>&nbsp;</span>
-    return <a href={value} target="_blank" className="with-ellipsis">More info</a>
+function Link({value, className}) {
+    if (!value) return <span className={className}>&nbsp;</span>
+    return <a href={value} target="_blank" className={classList("with-ellipsis", className || '')}>More info</a>
 }
 
 function DatasetList(props){
@@ -33,9 +34,12 @@ function DatasetList(props){
             else if (propertyKey === 'Parameter') {
                 return (<div title={value} >
                     <div className="with-ellipsis">{value}</div>
-                    {props.isActive && <div className={S.dropdown}>
+                    {/* To remove */}
+                    {/* {props.isActive && <div className={S.dropdown}>
+                        {headers.slice(1).map(header => <h5 key={header} className="with-ellipsis">{header}</h5>)}
                         {props.datasets.map(dataset => {
                             return <div className={S.dropdownRow}>
+                            return <>
                                 <span>&nbsp;</span>
                                 <span className="with-ellipsis" title={dataset['Name']}>{dataset['Name']}</span>
                                 <span className="with-ellipsis" title={dataset['type']}>{dataset['type']}</span>
@@ -46,8 +50,9 @@ function DatasetList(props){
                                 <span className="with-ellipsis" title={dataset['frequency']}>{dataset['frequency']}</span>
                                 <Link value={dataset['link']}/>
                             </div>
+                            </>
                         })}
-                    </div>}
+                    </div>} */}
                 </div>)
             }
             else if (propertyKey === 'Name') {
@@ -71,25 +76,36 @@ function DatasetList(props){
             
             return (<div title={value} className="with-ellipsis">{value}</div>)
         })}
+        {props.isActive && <>
+            {props.datasets.map(dataset => {
+                return <>
+                    <span className={S.dropdowned}>&nbsp;</span>
+                    <span className={classList('with-ellipsis', S.dropdowned, 'btn-link')} title={dataset['Name']}>{dataset['Name']}</span>
+                    <span className={classList('with-ellipsis', S.dropdowned)} title={dataset['type']}>{dataset['type']}</span>
+                    <span className={classList('with-ellipsis', S.dropdowned)} title={dataset['level']}>{dataset['level']}</span>
+                    <span className={classList('with-ellipsis', S.dropdowned)} title={dataset['resolution']}>{dataset['resolution']}</span>
+                    <span className={classList('with-ellipsis', S.dropdowned)} title={dataset['first obs']}>{dataset['first obs']}</span>
+                    <span className={classList('with-ellipsis', S.dropdowned)} title={dataset['last obs']}>{dataset['last obs']}</span>
+                    <span className={classList('with-ellipsis', S.dropdowned)} title={dataset['frequency']}>{dataset['frequency']}</span>
+                    <Link className={S.dropdowned} value={dataset['link']}/>
+                </>
+            })}
+        </>}
     </>)
 }
 
 export default function DatasetForm(props){
     const [datasets_per_parameter, setDatasetPerParameter] = useState({})
-    useEffect(() => {
-        setDatasetPerParameter(datasets_per_region[props.region]);
-    }, [props.region])
     const [activeParameter, setActiveParameter] = useState(null);
     const onParameterActivationHandler = useCallback((parameter) => {
         setActiveParameter(parameter);
     })
+    useEffect(() => {
+        setDatasetPerParameter(datasets_per_region[props.region]);
+        setActiveParameter(null);
+    }, [props.region])
 
-    return <fieldset 
-        className="regular-grid" 
-        style={{
-            position: 'relative',
-            '--description-grid__columns': 'minmax(60px, 1fr) 1fr min-content min-content min-content min-content min-content min-content min-content'
-        }}>
+    return <fieldset className={classList('regular-grid', S.root)}>
         {headers.map(header => <h4 key={header} className="with-ellipsis">{header}</h4>)}
         {datasets_per_parameter && Object
             .entries(datasets_per_parameter)
