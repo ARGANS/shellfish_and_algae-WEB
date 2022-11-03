3ec7426576db24d25c1e181c222404fae783a3c1
Nikolai Maltsev <nmaltsev@argans.eu>
2022-11-03 14:08:18 +0100

    No need for the PAR section for the Shellfish form

--- site/download.sh
+++ site/download.sh
@@ -1,7 +1,7 @@
 if [ "$3" = 'A' ]; then 
     columns='Temperature,eastward_Water_current,northward_Water_current,Nitrate,Ammonium,Phosphate,par'
 else
-    columns='Temperature,eastward_Water_current,northward_Water_current,Chlorophyll-a,par'
+    columns='Temperature,eastward_Water_current,northward_Water_current,Chlorophyll-a'
 fi
 
 curl -s $1 | node csv.js | node list.js "$columns" > $2
